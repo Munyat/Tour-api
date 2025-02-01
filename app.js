f68fa3cf8d06961c4b795c715177dde3cbfe18e3
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2025-02-01 16:42:44 +0300

    added error handling functions

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -15,6 +18,15 @@ app.use(express.static(`${__dirname}/public`));
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  // next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 //start server
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+//
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught exception');
+
+  process.exit(1);
+});
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
@@ -21,6 +29,15 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, '0.0.0.0', () => {
+const server = app.listen(port, '0.0.0.0', () => {
   console.log(`app running on port ${port}`);
+});
+
+//handles unhandles rejection
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('unhandled rejection');
+  server.close(() => {
+    process.exit(1);
+  });
 });
